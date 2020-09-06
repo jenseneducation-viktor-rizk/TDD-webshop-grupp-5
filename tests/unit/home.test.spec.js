0074ad6c1f8eaf7fdd8de16ca4042eecde0ffd71
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-06 15:35:07 +0200

    comment back and fix breaking test in home.test.spec.js

--- tests/unit/home.test.spec.js
+++ tests/unit/home.test.spec.js
@@ -1,11 +1,21 @@
-// const { shallowMount } = require("@vue/test-utils");
-// import Products from "@/components/Products.vue";
-// import Home from "@/views/Home.vue";
+const { shallowMount, createLocalVue } = require("@vue/test-utils");
+import VueRouter from "vue-router";
+import routes from "@/router/default.routes.js";
+import Products from "@/components/Products.vue";
+import Home from "@/views/Home.vue";
+import store from "@/store/index.js";
 
 it("should check if the products component is in home-view", () => {
-  //   const wrapper = shallowMount(Home, {
-  //   });
-  //   let actual = wrapper.findComponent(Products).exists();
-  //   let expected = true;
-  //   expect(actual).toBe(expected);
+  const localVue = createLocalVue();
+  const router = new VueRouter({ routes });
+  localVue.use(VueRouter);
+
+  const wrapper = shallowMount(Home, {
+    store,
+    localVue,
+    router,
+  });
+  let actual = wrapper.findComponent(Products).exists();
+  let expected = true;
+  expect(actual).toBe(expected);
 });
