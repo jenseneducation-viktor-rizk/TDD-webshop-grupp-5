53f5c85630fbd32221e1df3914046efc483f1453
Viktor Rizk <git config --global user.email>
2020-09-07 13:20:28 +0200

    filer products started

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -17,6 +17,9 @@ export default {
   props: {
     products: Array,
   },
+  computed: {
+    
+  },
   components: {
     ProductItem,
   },
---
+++ tests/unit/filter.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils';
+import FilterProducts from '@/components/FilterProducts.vue'
+describe("Products.vue", () => {
+    it("should show size M t-shirts if it's filtered by M", () => {
+      const wrapper = shallowMount(FilterProducts);
+      
+
+      expect()
+    })
+  })
